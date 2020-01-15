959168b60a36dd99befdbc5e4f52287490447d15
danial1021 <41097199+danial1021@users.noreply.github.com>
2020-01-15 21:03:14 +0900

    test
    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App';    
+// 브라우저 상에 React 컴포넌트를 보여주기 위해서 ReactDOM.render(렌더링 결과물, 그릴 DOM) 함수를 사용합니다.
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// public/index.html 파일에 있는 id가 'root'인 DOM을 찾아 App 컴포넌트를 렌더링 해줍니다.
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
