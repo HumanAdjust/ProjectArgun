71b4bef4140732734bad0918ad1e4a7dbc6339ee
danial1021 <41097199+danial1021@users.noreply.github.com>
2020-01-15 20:49:13 +0900

    test
    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import MainPage from './Main';
+import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<MainPage />);
+  const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
