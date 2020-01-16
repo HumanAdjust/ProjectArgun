03ecf48c22934aaa845025b3f8f66dc13d91984f
H_Apple |A.S.A.P| <39252325+HumanAdjust@users.noreply.github.com>
2020-01-16 21:00:47 +0900

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Home from './main/main'
+import Login from './user/Login';
+import SignUp from './user/SignUp';
+import NotFound from './404NotFound';
 
 function App() {
+  const title='Argun';
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <header>
+          <div class="Logo"><Link to="/">{title}</Link></div>
+          <div class="account">
+              <Link to="/Login">Login</Link> | <Link to="/SignUp">Sign Up</Link> | <a href="#">My Page</a>
+          </div>
+          <br />
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Login" component={Login} />
+            <Route path="/SignUp" component={SignUp} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+    </Router>
   );
 }
 
